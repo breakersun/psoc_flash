fa1e86c5d56c1ffd9530308735ff3e49478b1196
sunlong <sunlong@tcl.com>
2022-01-21 08:42:17 +0800

    add open/close port
    no error found on the real hardware at this time

--- psoc_flash.py
+++ psoc_flash.py
@@ -4,15 +4,39 @@ import PPCOM
 from PPCOM import enumInterfaces, enumFrequencies, enumSonosArrays
 
 
+class PortsError(RuntimeError):
+    pass
+
+
+def succeed(hr):
+    return hr >= 0
+
+
 class PSocFlashController(object):
     def __init__(self):
         self.programmer = win32com.client.Dispatch("PSoCProgrammerCOM.PSoCProgrammerCOM_Object")
 
     def open_port(self):
-        (_, (port, *_), *_) = self.programmer.GetPorts()
-        print(port)
+        (result, ports, last_result) = self.programmer.GetPorts()
+        if not succeed(result):
+            raise PortsError("Could not get any ports")
+
+        for port in ports:
+            if port.startswith("MiniProg4"):
+                (result, last_result) = self.programmer.OpenPort(port)
+                if succeed(result):
+                    return
+                else:
+                    raise PortsError("Could not open MiniProg4 port")
+            raise PortsError("Could not find MiniProg4 port")
+
+    def close_port(self):
+        (result, last_result) = self.programmer.ClosePort()
+        if not succeed(result):
+            raise PortsError("Could not close port")
 
 
 if __name__ == "__main__":
     p = PSocFlashController()
     p.open_port()
+    p.close_port()
